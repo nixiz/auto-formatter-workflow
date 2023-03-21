3e123571d5107241c377db623d2be9396765622c
Oguzhan KATLI <oguzhankatli@gmail.com>
2023-03-21 13:15:08 +0100

    removed timestamos

--- main.cpp
+++ main.cpp
@@ -10,8 +10,3 @@ int main(int argc, const char* argv[]) {
     std::cout << "first: " << first << "   second: " << second << "\n";
     return 0;
 }
-
-// Comment added on 2023-03-21 11:59:05
-
-
-// Comment added on 2023-03-21 12:13:34
